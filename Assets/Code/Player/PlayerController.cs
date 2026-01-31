cef72394d439ce279a2537d80a49a7d02a4e8f27
lcs <lcsfn@outlook.com>
2026-01-31 00:13:01 -0300

    Adicionando stats do player

--- Assets/Code/Player/PlayerController.cs
+++ Assets/Code/Player/PlayerController.cs
@@ -1,16 +1,57 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private PlayerControls controls;
+    private Rigidbody2D rb;
+    private Vector2 moveInput;
+
+    [Header("Movement Settings")]
+    public float moveSpeed = 5f;
+
+    private void Awake()
     {
+        // Initialize the PlayerControls instance
+        controls = new PlayerControls();
+
+        if (TryGetComponent<CharacterStats>(out CharacterStats stats))
+        {
+            moveSpeed = stats.movSpeed;
+        }
         
+        // Subscribe to the movement and jump actions
+        controls.Player.Move.performed += ctx => moveInput = 
+          ctx.ReadValue<Vector2>();
+        controls.Player.Move.canceled += ctx => moveInput = 
+          Vector2.zero;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        // Enable the input controls
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        // Disable the input controls when the player object is disabled
+        controls.Disable();
+    }
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        // Apply horizontal movement based on input
+        rb.linearVelocity = new Vector2(moveInput.x * moveSpeed, moveInput.y * moveSpeed);
     }
 }
