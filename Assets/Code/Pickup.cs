bb1ffb4bad45139852b5c6f5fe1d3bde34960378
lcs <lcsfn@outlook.com>
2026-01-31 05:16:58 -0300

    Alterando stats

--- Assets/Code/Pickup.cs
+++ Assets/Code/Pickup.cs
@@ -3,8 +3,12 @@ using UnityEngine;
 public class Pickup : MonoBehaviour
 {
 
-      void OnCollisionEnter(Collision collision)
+    void OnTriggerEnter2D(Collider2D col)
     {
-        
+        if (col.gameObject.CompareTag("Player"))
+        {
+            col.gameObject.GetComponent<StatManager>().AddHealth(10);
+            gameObject.SetActive(false);
+        }
     }
 }
