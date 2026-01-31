f623e5bb923484cb5bd1e0eb2919b13e4599b5dd
Daniel Vieira <daniel.vieira@smartgreen.net>
2026-01-31 00:15:21 -0300

    add: estrutura base de inimigos

---
+++ Assets/Code/Enemies/Behaviours/FollowPlayerEnemyBehaviour.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class FollowPlayerEnemyBehaviour : BaseEnemyBehaviour
+{
+    private Transform target;
+
+    public override void OnFixedUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnUpdate()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Code/Enemies/Enemy.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+public class Enemy
+{
+    [SerializeField] private Transform transform;
+    public Transform Transform { get { return transform; } }
+}
