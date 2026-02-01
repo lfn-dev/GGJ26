f243f899cec1edf460c2a6c1620f75a73e238d70
lcs <lcsfn@outlook.com>
2026-02-01 12:34:16 -0300

    First branch commit

---
+++ Assets/Code/Managers/LoadSceneManager.cs
@@ -0,0 +1,96 @@
+using System.Collections;
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneManager : SingletonSingleScene<LoadSceneManager>
+{
+    const string restartKeyWord = "restart";
+    const string quitKeyword = "quit";
+
+    public static float currentProgress {get; private set;} = 1;
+
+    public void LoadScene(string sceneName)
+    {
+        if(sceneName == restartKeyWord) sceneName = SceneManager.GetActiveScene().name;
+        else if (sceneName == quitKeyword) Application.Quit();
+        
+        if(currentProgress >= 1){
+            StartCoroutine(LoadSceneAsync(
+                sceneName,
+                (float progress)=> currentProgress = progress,
+                ()=> currentProgress = 1
+            ));
+        }
+    }
+
+    public void LoadScene(int sceneIndex)
+    {
+        if(currentProgress >= 1){
+            StartCoroutine(LoadSceneAsync(
+                sceneIndex,
+                (float progress)=> currentProgress = progress,
+                ()=> currentProgress = 1
+            ));
+        }
+    }
+
+    #region LoadScene
+/// <summary>
+/// Load the scene with a given name with the option for Actions to get the Progress
+/// as well as a method for when it has Completed.
+/// </summary>
+ 
+    public static IEnumerator LoadSceneAsync(string sceneName, Action<float> OnProgress, Action OnComplete)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+        operation.allowSceneActivation = false;
+    
+        while ((operation.progress / 0.9f) < 1f)
+        {
+            OnProgress(operation.progress / 0.9f);
+            yield return null;
+        }
+    
+        OnComplete();
+        operation.allowSceneActivation = true;
+    }
+
+    public static IEnumerator LoadSceneAsync(int sceneIndex, Action<float> OnProgress, Action OnComplete)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        operation.allowSceneActivation = false;
+    
+        while ((operation.progress / 0.9f) < 1f)
+        {
+            OnProgress(operation.progress / 0.9f);
+            yield return null;
+        }
+    
+        OnComplete();
+        operation.allowSceneActivation = true;
+    }
+    
+    public static IEnumerator LoadSceneAsync(string sceneName)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+        operation.allowSceneActivation = true;
+    
+        while ((operation.progress / 0.9f) < 1f)
+        {
+            yield return null;
+        }
+    }
+
+    public static IEnumerator LoadSceneAsync(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        operation.allowSceneActivation = true;
+    
+        while ((operation.progress / 0.9f) < 1f)
+        {
+            yield return null;
+        }
+    }
+    #endregion
+}
